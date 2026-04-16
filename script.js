20bee0be1129d10d04e472de9ffc7f20bca87034
AurickJD <126987244+AurocaAmaFarofa@users.noreply.github.com>
2026-04-16 11:03:05 -0300

    New page, new JS functions, new footer
    Added a Footer on the main page;
    New function to use "Local Storage";
    New popup when you click the button "order";
    A function was created to store the customer's order.

--- script.js
+++ script.js
@@ -45,3 +45,97 @@ document.addEventListener('DOMContentLoaded', () => {
 
   items.forEach((item) => observer.observe(item))
 })
+
+let itemAtual = null
+
+const botoes = document.querySelectorAll('.Item button')
+
+const modal = document.getElementById('modal')
+const titulo = document.getElementById('modalTitulo')
+const descricao = document.getElementById('modalDescricao')
+const preco = document.getElementById('modalPreco')
+
+botoes.forEach((btn) => {
+  btn.addEventListener('click', (e) => {
+    e.stopPropagation()
+
+    const item = btn.closest('.Item')
+
+    titulo.innerText = item.querySelector('h3').innerText
+    descricao.innerText = item.querySelector('p').innerText
+    preco.innerText = item.querySelector('.preco').innerText
+
+    modal.classList.add('active')
+  })
+})
+
+document.addEventListener('DOMContentLoaded', () => {
+  const fechar = document.getElementById('fechar')
+  const modal = document.getElementById('modal')
+
+  fechar.addEventListener('click', () => {
+    modal.classList.remove('active')
+  })
+})
+
+botoes.forEach((btn) => {
+  btn.addEventListener('click', () => {
+    const item = btn.closest('.Item')
+
+    itemAtual = {
+      nome: item.querySelector('h3').innerText,
+      descricao: item.querySelector('p').innerText,
+      preco: item.querySelector('.preco').innerText,
+    }
+
+    modal.classList.add('active')
+  })
+})
+
+const botaoAdicionar = document.getElementById('adicionarCarrinho')
+
+if (botaoAdicionar) {
+  botaoAdicionar.addEventListener('click', () => {
+    let carrinho = JSON.parse(localStorage.getItem('carrinho')) || []
+
+    carrinho.push(itemAtual)
+
+    localStorage.setItem('carrinho', JSON.stringify(carrinho))
+
+    alert('Item adicionado ao pedido!')
+
+    modal.classList.remove('active')
+  })
+}
+
+function irParaCarrinho() {
+  window.location.href = 'carrinho.html'
+}
+
+if (lista) {
+  const carrinho = JSON.parse(localStorage.getItem('carrinho')) || []
+
+  if (carrinho.length === 0) {
+    lista.innerHTML = '<p class="vazio">Seu carrinho está vazio</p>'
+  } else {
+    carrinho.forEach((item) => {
+      const div = document.createElement('div')
+      div.classList.add('item-carrinho')
+
+      div.innerHTML = `
+        <h3>${item.nome}</h3>
+        <p>${item.descricao}</p>
+        <span>${item.preco}</span>
+      `
+
+      lista.appendChild(div)
+    })
+  }
+}
+
+const carrinho = JSON.parse(localStorage.getItem('carrinho')) || []
+
+function limparCarrinho() {
+  localStorage.removeItem('carrinho')
+  location.reload()
+}
